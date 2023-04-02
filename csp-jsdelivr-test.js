c1c4218548c029456dca84eabd505e94a97f1e76
avilaaa <40517094+blackbeard666@users.noreply.github.com>
2023-04-02 21:47:04 +0800

    Update csp-jsdelivr-test.js

--- csp-jsdelivr-test.js
+++ csp-jsdelivr-test.js
@@ -1,2 +1,3 @@
-var img = new Image();
-img.src = "https://enj6wjy0rfwu.x.pipedream.net";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://enj6wjy0rfwu.x.pipedream.net/aaa", true);
+xhr.send();
